c0d5b1a181ac83474f98fab6e0e64b561b5e87a3
Ilshat Nasyrov <ilshatznasyrov@gmail.com>
2023-05-09 18:36:00 +0500

    Залил домашние задания по 1 и 2 вебинару

---
+++ first_lesson_nasyrov.py
@@ -0,0 +1,8 @@
+last_name = 'Насыров'
+first_name = 'Ильшат'
+work_experience = 1
+position = 'Инженер-тестировщик'
+
+print('Меня зовут ' + last_name, first_name + '. ')
+print('Мой стаж работы в компании Тензор', work_experience, 'год.')
+print('Моя должность', position + '.')
