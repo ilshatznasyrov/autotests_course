6bc1fa85dcb2ac933491764b148d71b5cffdaec2
Ilshat Nasyrov <ilshatznasyrov@gmail.com>
2023-06-03 18:33:26 +0500

    Решил задачи по 9 вебинару

---
+++ Homework9/task_1.py
@@ -0,0 +1,23 @@
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
+
+
+# Здесь пишем код
+file1 = open('test_file/task1_data.txt', 'r', encoding='utf8')
+file2 = open('test_file/task1_answer.txt', 'w', encoding='utf8')
+for i in file1:
+    file2.write(''.join([x for x in i if not x.isdigit()]))
+file1.close()
+file2.close()
+
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
